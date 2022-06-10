1a2f184704a4744471be982fbbcecda8067106bc
Elem <elem15@yandex.ru>
2022-06-10 14:55:44 +0300

    feat: create base navigation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import { argv } from 'process';
 import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from 'process';
+import { spawn } from 'node:child_process';
+import path from 'path';
+import os from 'os';
+import fsp from 'fs/promises';
+
+import Navigator from './Navigator.js'
 
 const rl = readline.createInterface({ input, output });
 
 const userName = argv[2].split('=')[1];
 
+const home = os.homedir();
+
+const navigator = new Navigator(home);
+
 console.log(`Welcome to the File Manager, ${userName}!`)
 rl.on('line', (input) => {
-  if(input.trim() === '.exit') {    
+  input = input.trim()
+  if(input === '.exit') {    
     rl.close();
   }
+  if(input === 'ls') {    
+    navigator.ls();
+  }
+  if(input === 'up') {    
+    navigator.up();
+  }
+  if(input.startsWith('cd ')) { 
+    const pathToDir = input.split(' ')[1];
+    navigator.cd(pathToDir);
+  }
+
 });
-rl.on('close', () => console.log(`Thank you for using File Manager, ${userName}!`))
+
+rl.on('close', () => console.log(`Thank you for using File Manager, ${userName}!`));
